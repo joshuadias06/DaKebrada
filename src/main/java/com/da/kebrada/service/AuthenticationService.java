93abe0423dbc6fdb105a7ec5f0bf8ca1ca8b6cc0
Joshua Dias <joshuadiasbf@gmail.com>
2025-02-22 11:44:02 -0300

    Created AuthenticationService class

---
+++ src/main/java/com/da/kebrada/service/AuthenticationService.java
@@ -0,0 +1,35 @@
+package com.da.kebrada.service;
+
+import com.da.kebrada.repository.UserRepository;
+import com.da.kebrada.security.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authManager;
+    private final UserDetailsService userDetailsService;
+
+    public AuthenticationService(UserRepository repository, JwtService jwtService,
+                                 AuthenticationManager authManager, UserDetailsService userDetailsService) {
+        this.repository = repository;
+        this.jwtService = jwtService;
+        this.authManager = authManager;
+        this.userDetailsService = userDetailsService;
+    }
+
+    public String authenticate(LoginDTO loginDTO) {
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDTO.email(), loginDTO.password())
+        );
+
+        UserDetails user = userDetailsService.loadUserByUsername(loginDTO.email());
+        return jwtService.generateToken(user.getUsername());
+    }
+}
