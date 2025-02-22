509644a17de491b66b180dad99a60605114caafb
Joshua Dias <joshuadiasbf@gmail.com>
2025-02-22 11:25:16 -0300

    Start Academy PI Project

---
+++ src/main/java/com/da/kebrada/DaKebradaApplication.java
@@ -0,0 +1,13 @@
+package com.da.kebrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaKebradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaKebradaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/da/kebrada/DaKebradaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da.kebrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaKebradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
