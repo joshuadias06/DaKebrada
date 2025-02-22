88bba33f31b19137cb49309be63db510cf5e10ba
Joshua Dias <joshuadiasbf@gmail.com>
2025-02-22 11:41:47 -0300

    Created JwtService class

---
+++ src/main/java/com/da/kebrada/security/JwtService.java
@@ -0,0 +1,37 @@
+package com.da.kebrada.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "minhaChaveSecreta";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 dia
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token, String userEmail) {
+        return extractEmail(token).equals(userEmail);
+    }
+}
